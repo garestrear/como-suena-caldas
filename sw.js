053ac7d83cdadbd05b7d2af7d0e42d5b9f178e15
garestrear <garestrear@unal.edu.co>
2026-09-17 20:26:26 -0500

    Add files via upload
    Primera Versión de cómo suena Caldas

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='suena-caldas-v1';const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
